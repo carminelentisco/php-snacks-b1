a48b5a997a6346fd208d120be9bb0ccf7ec33884
carminelentisco <carminelentisco@gmail.com>
2020-05-18 15:46:12 +0200

    Aggiunti punteggi dinamici, con possibilità di selezione del range di punteggio. Aggiunto Snack 2

--- index.php
+++ index.php
@@ -6,46 +6,66 @@
     <title>Snacks PHP</title>
 </head>
 <body>
-
-    <?php 
-    $matches = [
+    <!-- Snack 1 -->
+    <section>
         
-        [
-            'home' => 'Lakers',
-            'guest' => 'Chicago Bulls',
-            'pt_home' => 170,
-            'pt_guest' => 110
-        ],
-        [
-            'home' => 'Golden State',
-            'guest' => 'Boston Celtic',
-            'pt_home' => 100,
-            'pt_guest' => 150
-        ],
-        [
-            'home' => 'Toronto Raptors',
-            'guest' => 'Nev York Knicks',
-            'pt_home' => 20,
-            'pt_guest' => 35
-        ],
-        [
-            'home' => 'Brooklyn Nets',
-            'guest' => 'Detroit Pistons',
-            'pt_home' => 120,
-            'pt_guest' => 121
-        ],
+        <?php 
+        $min = 1;
+        $max = 200;
 
-    ];
+        $matches = [
+            
+            [
+                'home' => 'Lakers',
+                'guest' => 'Chicago Bulls',
+                'pt_home' => rand($min, $max),
+                'pt_guest' => rand($min, $max)
+            ],
+            [
+                'home' => 'Golden State',
+                'guest' => 'Boston Celtic',
+                'pt_home' => rand($min, $max),
+                'pt_guest' => rand($min, $max)
+            ],
+            [
+                'home' => 'Toronto Raptors',
+                'guest' => 'Nev York Knicks',
+                'pt_home' => rand($min, $max),
+                'pt_guest' => rand($min, $max)
+            ],
+            [
+                'home' => 'Brooklyn Nets',
+                'guest' => 'Detroit Pistons',
+                'pt_home' => rand($min, $max),
+                'pt_guest' => rand($min, $max)
+            ],
 
-    ?>
-    <h1><strong>NBA</strong> - Matches </h1>
-    <p>Olimpia Milano - Cantù | 55-60</p>
+        ];
+        ?>
 
-    <ul>
-        <?php for($i = 0; $i < count($matches); $i++) {?>
-        <li><?php echo $matches[$i]['home'] . ' ' . $matches[$i]['guest'] . ' | ' . $matches[$i]['pt_home'] . ' - ' . $matches[$i]['pt_guest'] ?></li>
-        <?php } ?>
-    </ul>
-    
+        <h1><strong>NBA</strong> - Matches </h1>
+
+        <ul>
+            <?php for($i = 0; $i < count($matches); $i++) {?>
+            <li><?php echo $matches[$i]['home'] . ' ' . $matches[$i]['guest'] . ' | ' . $matches[$i]['pt_home'] . ' - ' . $matches[$i]['pt_guest'] ?></li>
+            <?php } ?>
+        </ul>
+
+    </section>
+
+    <hr>
+    <!-- Snack 2 -->
+    <section>
+        
+        <?php 
+        $data = $_GET;
+        if ( (strlen($data['name']) <= 3) || ( (strpos($data['mail'], '@') === false) || (strpos($data['mail'], '.') === false) ) || is_numeric($data['age']) === false ) {
+            echo '<p>Accesso Negato </p>';
+        } else {
+            echo '<p>Accesso Consentito </p>';
+        }
+        ?>
+
+    </section>
 </body>
 </html>
